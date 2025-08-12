89e0e65b27655a6797f910c38ba62db05a05914b
sans-creator <sanskarj163@gmail.com>
2025-08-12 21:12:56 +0530

    App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 
 <!-- App.vue -->
 <template>
+<div class="container">
   <div>{{ msg }}</div>
-  <ContactUs />
-  <ButtonCounter />
-    <ButtonCounter />
+  <ContactUs name="Sanskar" :phone="123456" :ownerName="sans"
+  />
+</div>
 
 </template>
 
 <script setup>
+import { reactive } from 'vue'
 import ButtonCounter from './components/ButtonCounter.vue'
 import ContactUs from './components/ContactUs.vue' // if you have this component
 
 const msg = 'hello world' // use ref('hello world') if you plan to change it later
+const contactsOwner="sans@gmail.com"
+const contacts=reactive([
+    {
+        name:"asss",
+        phone:12332211,
+        ownerName:ownerName.value,
+        isFavourite:false
+    },{
+        name:"asddcd",
+        phone:1232211,
+        ownerName:ownerName.value,
+        isFavourite:true
+
+    },{
+        name:"ben",
+        phone:123221123211,
+        ownerName:ownerName.value,
+        isFavourite:false
+
+    }
+])
+
 </script>
 
 <style>
--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-        <h1>Contact Us
-        </h1>
-        <p>Please send an email to {{ email }}</p>
+    <div class="bg-info rounded p-4 m-2">
+        <h3>Name:{{ name }}
+        </h3>
+        <p> email  {{ email }}</p>
+        <p>Phone Number:{{ phone }}</p>
+        <p class="float-end small" v-if="ownerName!=''">
+            this conatct belong to {{ ownerName  }}
+        </p>
     </div>
 </template>
 
+<!-- we can bind props with local vaibale and propeties -->
+
 <script setup>
-import { ref } from 'vue'
+import { ref,defineProps } from 'vue'
 
 
-        const email=ref("hello.")
-
+        const email=ref("sanskarj163.") //local variable of email(more prefrence)
+        const props=defineProps({
+            name:{
+                type:String,
+                required:true
+            },
+            phone:Number,
+            ownerName:String
+        })
 
 </script>
